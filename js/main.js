25ec0076ed550473a83e75d44a65152dc583a106
Alan <alan88w@yahoo.com>
2018-11-14 19:36:32 +0800

    Add logic to create board and to reset game

--- js/main.js
+++ js/main.js
@@ -30,19 +30,60 @@ var checkForMatch = function() {
     }
 }
 
-var flipCard = function(cardId) {
-    
-    console.log("User flipped " + cards[cardId].rank);
-    console.log("User flipped " + cards[cardId].suit);
-    console.log("User flipped " + cards[cardId].cardImage);
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
+var flipCard = function() {
+    
+    var cardId = getRandomInt(4);
+    // this.getAttribute('data-id');
+    
     cardsInPlay.push(cards[cardId].rank);
+
+    this.setAttribute('src', cards[cardId].cardImage);
 
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
 }
 
-flipCard(0);
-flipCard(2);
+var resetBoard = function() {
+
+    imgList = document.querySelectorAll('img');
+    
+    // for (var i = 0; i < imgList.length; i++) {
+    //     imgList[i].src= "images/back.png";
+    // };
+
+    // Can also do this
+
+    // for (var item of imgList) {
+    //     item.src= "images/back.png";
+    // }
+
+
+    Array.prototype.forEach.call(imgList, function (item) {
+        item.src= "images/back.png";
+      });
+
+    cardsInPlay = [];
+}
+var createBoard = function() {
+  
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', "images/back.png");
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+
+}
+
+createBoard();
+
+btn = document.getElementById('reset');
+
+btn.addEventListener('click', resetBoard);
 
